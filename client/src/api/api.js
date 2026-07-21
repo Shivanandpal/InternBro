73c53ec828e7fbad4cf051e24803bf4e8cdea4c7
Himanshi <himanshipal74@gmail.com>
2026-07-21 21:16:34 +0530

    render added

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://internbro.onrender.com/",
+    baseURL: "https://internbro.onrender.com",
     headers: {
         "Content-Type": "application/json",
     }
